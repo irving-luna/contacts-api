586c71d1dd6347a2a1839cb9a0d3a981245da34e
Irving Luna <irving.luna.ram+github@gmail.com>
2024-05-03 11:05:49 -0600

    Add unit test

--- usecase/contact_test.go
+++ usecase/contact_test.go
@@ -10,16 +10,20 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+var (
+	expected = model.Contact{
+		ID:    "aaaa",
+		Name:  "pepe",
+		Phone: "123456",
+	}
+)
+
 func TestContact_Get(t *testing.T) {
 	type args struct {
 		ctx       context.Context
 		contactID string
 	}
-	var(
-		expected = model.Contact{
-			ID: "aaaa",
-		}
-	)
+
 	tests := []struct {
 		name    string
 		args    args
@@ -33,6 +37,7 @@ func TestContact_Get(t *testing.T) {
 				contactID: "aaaa",
 			},
 			want: &expected,
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
@@ -52,6 +57,51 @@ func TestContact_Get(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Contact.Get() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestContact_Create(t *testing.T) {
+	
+	type args struct {
+		ctx     context.Context
+		contact model.Contact
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *model.Contact
+		wantErr bool
+	}{
+		{
+			name: "Positive scenario - create contact",
+			args: args{
+				ctx: context.Background(),
+				contact: model.Contact{
+					Name: "pepe",
+					Phone: "123456",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			contactRepo := mocks.ContactRepository{}
+			contactRepo.On("Create", mock.Anything, mock.Anything).Return(
+				tt.want, nil,
+			)
+			u := &Contact{
+				contactRepo: &contactRepo,
+			}
+			got, err := u.Create(tt.args.ctx, tt.args.contact)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Contact.Create() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Contact.Create() = %v, want %v", got, tt.want)
 			}
 		})
 	}
